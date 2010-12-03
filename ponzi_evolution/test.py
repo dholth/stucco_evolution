9aac19781ba447244d6131dea4159e45203fb057
Daniel Holth <dholth@fastmail.fm>
2010-12-02 23:36:10 -0500

    100% test coverage

--- ponzi_evolution/test.py
+++ ponzi_evolution/test.py
@@ -1,10 +1,9 @@
 import ponzi_evolution
+import sqlalchemy
+import sqlalchemy.orm
 
 def test_evolve():
-    import sqlalchemy
-    import sqlalchemy.orm
-    import ponzi_evolution
-    engine = sqlalchemy.create_engine('sqlite:///test.db', echo=True)
+    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
     session = Session()
     ponzi_evolution.initialize(session)
@@ -13,3 +12,12 @@ def test_evolve():
     ponzi_evolution.PONZI_EVOLUTION_SCHEMA_VERSION = 1
     ponzi_evolution.upgrade(session)
     session.commit()
+
+def test_unversioned():
+    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+    ponzi_evolution.initialize(session)
+    manager = ponzi_evolution.SQLAlchemyEvolutionManager(session, 'testing_testing', 4)
+    assert manager.get_db_version() is None
+
