3c80ccdc69235e6f6cd76f71259ee1375274a0b1
Daniel Holth <dholth@fastmail.fm>
2010-12-02 23:23:43 -0500

    initial commit

---
+++ ponzi_evolution/test.py
@@ -0,0 +1,14 @@
+import ponzi_evolution
+
+def test_evolve():
+    import sqlalchemy
+    import sqlalchemy.orm
+    import ponzi_evolution
+    engine = sqlalchemy.create_engine('sqlite:///test.db', echo=True)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+    ponzi_evolution.initialize(session)
+    ponzi_evolution.upgrade(session)
+
+    ponzi_evolution.PONZI_EVOLUTION_SCHEMA_VERSION = 1
+    ponzi_evolution.upgrade(session)
